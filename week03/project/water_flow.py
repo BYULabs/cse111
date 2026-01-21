cf68e936bf88113c9a18cafd54cbef76da21b3ea
Sebastian Iturralde <itusebastian@gmail.com>
2026-01-21 16:30:20 -0500

    Add tests for water flow calculations and implement pressure gain function

--- week03/project/water_flow.py
+++ week03/project/water_flow.py
@@ -37,12 +37,11 @@ def water_column_height(tower_height, tank_height):
     return tower_height + 3 * tank_height / 4
 
 def pressure_gain_from_water_height(height):
-    #TODO: Need to implement
-    return 0
+    return WATER_DENSITY * 9.80665 * height / 1000
 
 def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity):
     numerator = -friction_factor * pipe_length * WATER_DENSITY * fluid_velocity ** 2
-    denominator = 200 * pipe_diameter
+    denominator = 2000 * pipe_diameter
     return numerator / denominator
 
 def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
