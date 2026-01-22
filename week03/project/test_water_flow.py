c837410515d78600cba900bcc6b4ee000b2d838f
Sebastian Iturralde <itusebastian@gmail.com>
2026-01-22 09:12:56 -0500

    Add tests for pressure conversion and implement convert_kpa_to_psi function

--- week03/project/test_water_flow.py
+++ week03/project/test_water_flow.py
@@ -1,4 +1,4 @@
-from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe, pressure_loss_from_fittings, reynolds_number, pressure_loss_from_pipe_reduction
+from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe, pressure_loss_from_fittings, reynolds_number, pressure_loss_from_pipe_reduction, convert_kpa_to_psi
 from pytest import approx
 import pytest
 
@@ -40,6 +40,12 @@ def test_pressure_loss_from_pipe_reduction():
     assert pressure_loss_from_pipe_reduction(0.28687, 0, 1, 0.048692) == approx(0, abs=0.001)
     assert pressure_loss_from_pipe_reduction(0.28687, 1.65, 471729, 0.048692) == approx(-163.744, abs=0.001)
     assert pressure_loss_from_pipe_reduction(0.28687, 1.75, 500318, 0.048692) == approx(-184.182, abs=0.001)
+
+def test_convert_kpa_to_psi():
+    assert convert_kpa_to_psi(0) == approx(0, abs=0.01)
+    assert convert_kpa_to_psi(100) == approx(14.5038, abs=0.01)
+    assert convert_kpa_to_psi(200) == approx(29.0076, abs=0.01)
+    assert convert_kpa_to_psi(295.628) == approx(42.879, abs=0.01)
 
 # Call the main function that is part of pytest so that the
 # computer will execute the test functions in this file.
